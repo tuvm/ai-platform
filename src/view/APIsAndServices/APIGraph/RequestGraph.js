02994b8aea39888daa494caf7c1401875c4b038c
Trung Nguyen <trung1704ptit@gmail.com>
2021-01-21 17:17:17 +0700

    styling main height

--- src/view/APIsAndServices/APIGraph/RequestGraph.js
+++ src/view/APIsAndServices/APIGraph/RequestGraph.js
@@ -21,7 +21,7 @@ const data = {
 
 export default function RequestGraph() {
   return (
-    <div style={{ height: 500 }}>
+    <div style={{ height: 350 }}>
       <Bar
         data={data}
         width={50}
