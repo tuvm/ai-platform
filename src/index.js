7e78a7c462efd0df978514df36378b409c25a026
Trung Nguyen <trung1704ptit@gmail.com>
2021-09-07 14:47:18 +0700

    rm base dashboard

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./configStore";
-import { BASE_FE_PREFIX } from "./utils/constants/config";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
@@ -14,7 +13,7 @@ window.BUILD_INFO = process.env.BUILD_TIME + "-" + process.env.BUILD_USERNAME;
 ReactDOM.render(
   <React.Fragment>
     <Provider store={store}>
-      <BrowserRouter basename={BASE_FE_PREFIX}>
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
