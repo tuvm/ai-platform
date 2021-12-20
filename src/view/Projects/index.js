7ad33e4bd3c5c3ee1ec0d3f8eb9624569864eedb
vutrungdungbk@gmail.com <vutrungdungbk@gmail.com>
2021-12-20 09:20:51 +0700

    reload projects that in processing status

--- src/view/Projects/index.js
+++ src/view/Projects/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProjectList from './ProjectList';
 import RecentProjects from './RecentProjects'
 import Devider from '../../components/devider';
 // import { actionGetProjectList } from './actions';
 // import Loading from '../../components/loading/Loading';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+
+import get from 'lodash/get';
 
 import './ProjectStyle.scss';
+import { actionGetProjectList } from './actions';
 
 export default function Projects() {
   const data = useSelector(state => state.system.projectList)
-  // const dispatch = useDispatch()
+  const maxReload = 3;
+  const [numberReload, setNumberReload] = useState(maxReload);
+  const dispatch = useDispatch()
   // const loading = useSelector(state => state.system.projectListLoading)
 
   // useEffect(() => {
@@ -20,6 +25,25 @@ export default function Projects() {
   // if (loading || !data) {
   //   return <Loading />;
   // }
+
+  useEffect(() => {
+    let processingProjects = get(data, 'recent.data', []).filter(p => p.provision_status === 'processing');
+    let timer = null;
+    if(processingProjects.length > 0){
+      console.log('Has processing project', numberReload);
+      if(numberReload > 0){
+        setNumberReload(numberReload-1);
+        timer = setTimeout(()=> {
+          dispatch(actionGetProjectList());
+        }, 2000);
+      }
+    }else{
+      setNumberReload(maxReload);
+    }
+    if(timer){
+      return () => clearTimeout(timer);
+    }
+  }, [data])
 
   return (
     <div className="project-page-inner">
