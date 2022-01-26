d5586ea0f81fc115cbf2a42c3c7270651854c7d0
Vu Minh Tu <tu.vm41@gmail.com>
2022-01-26 16:08:26 +0700

    Update logo

--- src/components/Helmet/index.js
+++ src/components/Helmet/index.js
@@ -1,30 +1,32 @@
-import React, { useEffect, useState } from "react";
-import { Helmet } from "react-helmet";
-import { APP_ROUTES } from "../../utils/constants/config";
-import { useTranslation } from "react-i18next";
-import get from "lodash/get";
-import { withRouter } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import { APP_ROUTES } from '../../utils/constants/config';
+import { useTranslation } from 'react-i18next';
+import get from 'lodash/get';
+import { withRouter } from 'react-router-dom';
 
 function AppHelmet(props) {
   const { t } = useTranslation();
   const { location } = props;
-  const [pageName, setPageName] = useState("");
+  const [pageName, setPageName] = useState('');
 
   useEffect(() => {
-    const pathname = get(location, 'pathname') || '';
+    const path = get(location, 'pathname') || '';
+    const pathname = '/' + (path.split('/').reverse()[0] || '');
     const filter = APP_ROUTES.find((item) => item.pathname === pathname);
-    const name = get(filter, "name");
+    const name = get(filter, 'name');
     setPageName(name);
   }, [location]);
 
   return (
     <Helmet>
       <meta charSet="utf-8" />
-      <title>{t('IDS_APP_NAME')} - {t(pageName)}</title>
+      <title>
+        {t('IDS_APP_NAME')} - {t(pageName)}
+      </title>
       <link rel="canonical" href="https://vindr.ai" />
     </Helmet>
   );
 }
-
 
 export default withRouter(AppHelmet);
--- src/view/APIKeys/GenerateToken.js
+++ src/view/APIKeys/GenerateToken.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import { Form, Input, Button } from "antd";
+import React, { useState } from 'react';
+import { Form, Input, Button } from 'antd';
 import { Checkbox, message } from 'antd';
-import { Row, Col, notification } from "antd";
+import { Row, Col, notification } from 'antd';
 import { CopyOutlined, PlusOutlined } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
 import get from 'lodash/get';
 import { API_SCOPES } from '../../utils/constants/config';
 import { actionGenerateToken, actionGetAPIKeys } from './actions';
 
-import "./GenerateToken.scss";
+import './GenerateToken.scss';
 
 export default function GenerateToken() {
   const [form] = Form.useForm();
@@ -18,13 +18,13 @@ export default function GenerateToken() {
 
   const onFinish = async () => {
     const name = form.getFieldValue('key_name');
-    const scopes = form.getFieldValue('scope_list')
+    const scopes = form.getFieldValue('scope_list');
     const payload = {
       scope: scopes,
       expiry_time: 0,
-      description: "",
-      name
-    }
+      description: '',
+      name,
+    };
 
     setLoading(true);
 
@@ -34,8 +34,7 @@ export default function GenerateToken() {
       setToken(apiKey);
       setLoading(false);
       notification.success({
-        description:
-          t('IDS_API_KEY_CREATE_SUCCESS'),
+        description: t('IDS_API_KEY_CREATE_SUCCESS'),
       });
       actionGetAPIKeys();
       form.resetFields();
@@ -43,15 +42,18 @@ export default function GenerateToken() {
       setLoading(false);
       notification.error({ description: t('IDS_ERROR_MESSAGE') });
     }
-  }
+  };
 
   const handleCopy = () => {
-    navigator.clipboard.writeText(token).then(function () {
-      message.success('Copied API key');
-    }, function (err) {
-      message.error('Could not copy API Key: ');
-    });
-  }
+    navigator.clipboard.writeText(token).then(
+      function () {
+        message.success('Copied API key');
+      },
+      function (err) {
+        message.error('Could not copy API Key: ');
+      }
+    );
+  };
 
   return (
     <Row gutter={16}>
@@ -59,59 +61,72 @@ export default function GenerateToken() {
         <div className="generate-token-left-side">
           <h4>API key generator</h4>
           <p>
-            You can generate a personal API key for each application you
-            use that needs access to the VinDr AI Platform.
+            You can generate a personal API key for each application you use
+            that needs access to the VinDr CAD Platform.
           </p>
         </div>
       </Col>
       <Col sm={24} md={17}>
         <div className="generate-token-right-side">
-          <Form
-            form={form}
-            layout="vertical"
-            onFinish={onFinish}
-          >
+          <Form form={form} layout="vertical" onFinish={onFinish}>
             <Form.Item
               label="Name of API key"
               required
               tooltip="This is a required field"
               className="title"
               name="key_name"
-              rules={[{ required: true, message: 'Please input your API Key name' }]}
+              rules={[
+                { required: true, message: 'Please input your API Key name' },
+              ]}
             >
               <Input />
             </Form.Item>
 
             <div className="section">
               <div className="title">Scopes</div>
-              <Form.Item name="scope_list" valuePropName="checked" rules={[{ required: true, message: 'Please select at least one API scope' }]}>
+              <Form.Item
+                name="scope_list"
+                valuePropName="checked"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please select at least one API scope',
+                  },
+                ]}
+              >
                 <Checkbox.Group>
-                  {API_SCOPES && API_SCOPES.map(item => (
-                    <Checkbox key={item.key} value={item.key}>
-                      <span>{item.name}</span>
-                      <p className="sub-checkbox">
-                        {t(item.description)}
-                      </p>
-                    </Checkbox>
-                  ))}
+                  {API_SCOPES &&
+                    API_SCOPES.map((item) => (
+                      <Checkbox key={item.key} value={item.key}>
+                        <span>{item.name}</span>
+                        <p className="sub-checkbox">{t(item.description)}</p>
+                      </Checkbox>
+                    ))}
                 </Checkbox.Group>
               </Form.Item>
             </div>
 
             <div className="section generate-form">
               <Form.Item>
-                <Button type="primary" htmlType="submit" icon={<PlusOutlined />} loading={loading}>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  icon={<PlusOutlined />}
+                  loading={loading}
+                >
                   {t('IDS_CREATE_API_KEY')}
                 </Button>
               </Form.Item>
               <div className="generate-input">
                 <Input value={token} />
-                <Button onClick={handleCopy}><CopyOutlined /></Button>
+                <Button onClick={handleCopy}>
+                  <CopyOutlined />
+                </Button>
               </div>
             </div>
           </Form>
         </div>
       </Col>
-    </Row >
+    </Row>
   );
 }
